ed3fe073233b71dd20d923f4e843abc7ede14d0e
claudechow <claudesoft@163.com>
2020-04-20 14:46:49 +0800

    Lession4
    vue文件标准结构、v-for遍历、key、v-bind属性、@click事件、绑定class、基础数据类型、引用数据类型区别

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -18,8 +18,27 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
- #login{
-     height: 100vh;
+ #login {
+     height: 100vh; 
      background-color: #344a5f;
+ }
+ .login-wrap {
+     widows: 33px;
+     margin: auto;
+ }
+ .menu-tab {
+     text-align: center;
+     li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: #fff;
+        border-radius: 2px;
+        cursor: pointer;
+     }
+ }
+ .current {
+     background-color: rgba(0,0,0,.1);
  }
 </style>
