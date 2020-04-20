d6a74043b51f9a49bf19bc1f705b43a2770d57bb
claudechow <claudesoft@163.com>
2020-04-20 14:47:46 +0800

    Lession4
    vue文件标准结构、v-for遍历、key、v-bind属性、@click事件、绑定class、基础数据类型、引用数据类型区别

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,20 +1,41 @@
 <template>
     <div id="login">
-        登录页
-        <el-row>
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-        </el-row>
+        {{title}}
+      <div class="login-wrap">
+          <ul class="menu-tab">
+              <li v-for="item in menuTab" :key="item.id" :class="{'current': item.isCurrent}" @click="toggleMenu(item)">{{item.txt}}</li>
+          </ul>
+      </div>
     </div>
 
 </template>
 <script>
 export default {
-    name: "login"
+    name: "login",
+    data(){
+        return{
+            title:'登录页面',
+            menuTab: [
+                {txt: '登录',isCurrent: true },
+                {txt: '注册',isCurrent: false}
+            ],
+            
+        }
+    },
+    created(){},
+
+    //挂载完成后自动执行
+    mounted(){},
+
+    //事件函数
+    methods: {
+        toggleMenu(data){
+            this.menuTab.forEach(element => {
+                element.isCurrent = false
+            });
+            data.isCurrent = true
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
